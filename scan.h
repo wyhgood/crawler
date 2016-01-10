9ed6ddc07bca286f77a7cc8c1b7ead86814fd39f
Wangzi <756651720@qq.com>
2016-01-10 02:20:19 -0800

    update

--- scan.h
+++ scan.h
@@ -3,12 +3,23 @@
 
 
 #include <iostream>
+#include <queue>
+#include <mutex>
 using namespace std;
 class SCAN{
  public:
-  SCAN(){};
+  mutex mtx;
+  queue<string> tasks;
+  
+  SCAN(){
+    for(int i=0; i<=1000; i++){
+      string ip = "117.135.250.133:80";
+      tasks.push(ip);
+    }    
+  };
   ~SCAN(){};
   
+  void check_ip();
   int http_get(string ip);
   //size_t write_to_string(void *ptr, size_t size, size_t count, void *stream);
 };
